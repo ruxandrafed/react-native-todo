80df8eaf66597c4c87cd3a47b5ae8127da75c67d
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-28 22:45:18 -0800

    Add checkmark next to the text input

--- header.js
+++ header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 class Header extends Component {
   render() {
     return (
       <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.toggleIcon}>{String.fromCharCode(10003)}</Text>
+        </TouchableOpacity>
         <TextInput
           value = {this.props.value}
           onChangeText={this.props.onChange}
@@ -25,6 +28,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center"
+  },
+  toggleIcon: {
+    fontSize: 30,
+    color: "#CCC",
+    marginRight: 10
   },
   input: {
     flex: 1,
