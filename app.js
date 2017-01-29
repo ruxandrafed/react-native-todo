cb07e6e344db8e4f48b1080a3d3800d86496b2eb
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-28 21:59:18 -0800

    Add some basic styles + ios specific using Platform

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text, Stylesheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import Header from "./header";
+import Footer from "./footer";
 
-export const App extends Component() {
-  state = {};
-  
+class App extends Component {
   render() {
     return (
-      
-    }
+      <View style={styles.container}>
+        <Header/>
+        <View style={styles.content}>
+          <Text>Content</Text>
+        </View>
+        <Footer/>
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    ...Platform.select({
+      ios: {
+        paddingTop: 30
+      }
+    })
+  },
+  content: {
+    flex: 1
+  }
+});
+
+export default App;
