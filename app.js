7839618781ee924e05057cb9496c39fe9d5a69ad
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-01-28 21:35:56 -0800

    First commit - basic content

---
+++ app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+export const App extends Component() {
+  state = {};
+  
+  render() {
+    return (
+      
+    }
+  }
+}
